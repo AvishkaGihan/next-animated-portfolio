9811aebe9349d169295da160795950495265d053
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-10-11 19:09:18 +0530

    Give Active Link Style for a Navbar Link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
+import NavLink from "./navLink";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -15,13 +16,11 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:48 text-xl">
       {/* Links */}
-      <div className="hidden md:flex gap-4 w-1/3">
+      <div className="hidden md:flex gap-4 lg:w-1/3">
         {links.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
@@ -37,7 +36,7 @@ const Navbar = () => {
         </Link>
       </div>
       {/* SOCIAL */}
-      <div className="hidden md:flex gap-4 w-1/3">
+      <div className="hidden md:flex gap-4 lg:w-1/3">
         <Link href="github.com">
           {" "}
           <Image src="/github.png" alt="" width={24} height={24} />
